1296d30a578e3ae377571d2e86519f5981db06a9
Boyko Alexey <elorian@yandex.ru>
2022-05-11 18:07:49 +0300

    Add state code

---
+++ tel_bot.py
@@ -0,0 +1,93 @@
+import os
+import logging
+import redis
+from dotenv import load_dotenv
+
+from telegram.ext import Filters, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+_database = None
+
+
+def start(bot, update):
+    """
+    Хэндлер для состояния START.
+
+    Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
+    Теперь в ответ на его команды будет запускаеться хэндлер echo.
+    """
+    update.message.reply_text(text='Привет!')
+    return "ECHO"
+
+
+def echo(bot, update):
+    """
+    Хэндлер для состояния ECHO.
+
+    Бот отвечает пользователю тем же, что пользователь ему написал.
+    Оставляет пользователя в состоянии ECHO.
+    """
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return "ECHO"
+
+
+def handle_users_reply(update, bot):
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id)
+        print(user_state)
+
+    states_functions = {
+        'START': start,
+        'ECHO': echo
+    }
+    state_handler = states_functions[user_state]
+    # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
+    # Оставляю этот try...except, чтобы код не падал молча.
+    # Этот фрагмент можно переписать.
+    try:
+        next_state = state_handler(bot, update)
+        db.set(chat_id, next_state)
+        print(db.get(chat_id))
+    except Exception as err:
+        print(err)
+
+
+def get_database_connection():
+    """
+    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
+    """
+    global _database
+    if _database is None:
+        #database_password = os.getenv("DATABASE_PASSWORD", 123)
+        database_host = os.getenv("DATABASE_HOST", 'localhost')
+        database_port = os.getenv("DATABASE_PORT", 6379)
+        _database = redis.Redis(host=database_host, port=database_port,
+                                decode_responses=True)
+                                #password=database_password)
+    return _database
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    tg_token = os.getenv("TELEGRAM_TOKEN")
+    updater = Updater(tg_token)
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+
+    updater.start_polling()
+    updater.idle()
