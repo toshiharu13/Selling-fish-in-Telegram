a9dd18cf269625eda44ce5878622a3293630252d
Boyko Alexey <elorian@yandex.ru>
2022-06-09 14:16:11 +0300

    Correct check fo error in HTTP requests

--- elasticpath_api.py
+++ elasticpath_api.py
@@ -18,7 +18,10 @@ def get_products():
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    return response.json()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def get_product_by_id(prod_id):
@@ -27,7 +30,10 @@ def get_product_by_id(prod_id):
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    return response.json()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def get_image_by_id(img_id):
@@ -36,8 +42,10 @@ def get_image_by_id(img_id):
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-
-    img = response.json()['data']['link']['href']
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    img = decoded_response['data']['link']['href']
     return img
 
 
@@ -49,9 +57,14 @@ def get_token():
         'grant_type': 'implicit'}
     if time.time() > TOKEN_EXPIRES:
         response = requests.post('https://api.moltin.com/oauth/access_token',
-                                 data=data).json()
-        TOKEN_EXPIRES = response['expires']
-        SHOP_TOKEN = response['access_token']
+                                 data=data)
+        response.raise_for_status()
+        decoded_response = response.json()
+        if 'error' in decoded_response:
+            raise requests.exceptions.HTTPError(decoded_response['error'])
+
+        TOKEN_EXPIRES = decoded_response['expires']
+        SHOP_TOKEN = decoded_response['access_token']
     return SHOP_TOKEN
 
 
@@ -65,7 +78,11 @@ def add_product_to_cart(product_id, quantity, cart_id='card_id'):
 
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
     response = requests.post(url, headers=headers, json=payload)
-    return response.json()
+    response.raise_for_status()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def get_products_in_cart(card_id):
@@ -73,7 +90,11 @@ def get_products_in_cart(card_id):
     url = f'https://api.moltin.com/v2/carts/{card_id}/items'
 
     response = requests.get(url, headers=headers)
-    return response.json()
+    response.raise_for_status()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def get_cart_total(cart_id):
@@ -82,7 +103,10 @@ def get_cart_total(cart_id):
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    return response.json()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def remove_cart_item(cart_id, product_id):
@@ -91,7 +115,10 @@ def remove_cart_item(cart_id, product_id):
 
     response = requests.delete(url, headers=headers)
     response.raise_for_status()
-    return response.json()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
 
 
 def create_customer(user_name, user_email):
@@ -105,5 +132,7 @@ def create_customer(user_name, user_email):
 
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
-
-    return response.json()
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return decoded_response
